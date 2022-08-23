0ba4636fdbcfafdd00a6700049725d7e0b679735
David B. Collins <david.collins.hou@gmail.com>
2022-08-23 15:57:29 -0500

    adding sql filtering functions

--- src/main/java/MySQLVerbsDAO.java
+++ src/main/java/MySQLVerbsDAO.java
@@ -1,12 +1,8 @@
-import Config.Config;
-
 //import java.sql.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import com.mysql.cj.jdbc.Driver;
-import java.util.Properties;
-import java.util.logging.Logger;
 
 public class MySQLVerbsDAO implements VerbsDAO {
 
@@ -75,6 +71,7 @@ public class MySQLVerbsDAO implements VerbsDAO {
             v.setThirdPart(rs.getString("ThirdPart"));
             v.setSupine(rs.getString("Supine"));
             v.setDeponency(rs.getString("Deponency"));
+            v.setEnglishMeanings(rs.getString("EnglishMeanings"));
             v.setNotes(rs.getString("Notes"));
 
             allVerbs.add(v);
@@ -91,8 +88,8 @@ public class MySQLVerbsDAO implements VerbsDAO {
         Verb v = new Verb();
         try {
             PreparedStatement ps = conn.prepareStatement("Select * from VerbList where id = ?");
-            ResultSet rs = ps.executeQuery();
             ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
             rs.next();
 
             v.setId(rs.getInt("id"));
@@ -102,7 +99,7 @@ public class MySQLVerbsDAO implements VerbsDAO {
             v.setSupine(rs.getString("Supine"));
             v.setConjugation(rs.getString("Conjugation"));
             v.setDeponency(rs.getString("Deponency"));
-
+            v.setEnglishMeanings(rs.getString("EnglishMeanings"));
             v.setNotes(rs.getString("Notes"));
 
             rs.close();
@@ -113,7 +110,7 @@ public class MySQLVerbsDAO implements VerbsDAO {
         return v;
     }
 
-    public List<Verb>filteredList(String strMatch) {
+    public List<Verb> filteredList(String strMatch) {
         ArrayList<Verb> someVerbs = new ArrayList<>();
         try {
             PreparedStatement ps = conn.prepareStatement("Select * from verbs where FirstPart = " + strMatch);
@@ -135,11 +132,105 @@ public class MySQLVerbsDAO implements VerbsDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
         return someVerbs;
     }
+
+    public List<Verb> filterByLetter(String strInitial) {
+        ArrayList<Verb> someVerbs = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement(sqlByLetter(strInitial));
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Verb v = new Verb();
+                v.setId(rs.getInt("id"));
+                v.setFirstPart(rs.getString("FirstPart"));
+                v.setSecondPart(rs.getString("SecondPart"));
+                v.setThirdPart(rs.getString("ThirdPart"));
+                v.setSupine(rs.getString("Supine"));
+                v.setDeponency(rs.getString("Deponency"));
+                v.setNotes(rs.getString("Notes"));
+                someVerbs.add(v);
+
+                rs.close();
+                ps.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return someVerbs;
+    }
+
+    public String sqlByLetter(String initialLetter) {
+        String initialLetterConvert;
+        String basicSelect = "Select * from VerbList";
+        switch (initialLetter) {
+            case "All":
+                return basicSelect;
+            case "A":
+                initialLetterConvert = " Where FirstPart like 'a%' or FirstPart like 'ā%' or FirstPart like 'â%'";
+                break;
+            case "B":
+                initialLetterConvert = " Where FirstPart like 'b%'";
+                break;
+            case "C":
+                initialLetterConvert = " Where FirstPart like 'c%'";
+                break;
+            case "D":
+                initialLetterConvert = " Where FirstPart like 'd%'";
+                break;
+            case "E":
+                initialLetterConvert = " Where FirstPart like 'e%' or FirstPart like 'ē%' or FirstPart like 'ê%'";
+                break;
+            case "F":
+                initialLetterConvert = " Where FirstPart like 'f%'";
+                break;
+            case "G":
+                initialLetterConvert = " Where FirstPart like 'g%'";
+                break;
+            case "H":
+                initialLetterConvert = " Where FirstPart like 'h%'";
+                break;
+            case "I":
+                initialLetterConvert = " Where FirstPart like 'i%' or FirstPart like 'ī%' or FirstPart like 'î%' or FirstPart like 'j%'";
+                break;
+            case "L":
+                initialLetterConvert = " Where FirstPart like 'l%'";
+                break;
+            case "M":
+                initialLetterConvert = " Where FirstPart like 'm%'";
+                break;
+            case "N":
+                initialLetterConvert = " Where FirstPart like 'n%'";
+                break;
+            case "O":
+                initialLetterConvert = " Where FirstPart like 'o%' or FirstPart like 'ō%' or FirstPart like 'ô%'";
+                break;
+            case "P":
+                initialLetterConvert = " Where FirstPart like 'p%'";
+                break;
+            case "Q":
+                initialLetterConvert = " Where FirstPart like 'q%'";
+                break;
+            case "R":
+                initialLetterConvert = " Where FirstPart like 'r%'";
+                break;
+            case "S":
+                initialLetterConvert = " Where FirstPart like 's%'";
+                break;
+            case "T":
+                initialLetterConvert = " Where FirstPart like 't%'";
+                break;
+            case "U":
+                initialLetterConvert = " Where FirstPart like 'u%' or FirstPart like 'ū%' or FirstPart like 'û%' or FirstPart like 'v%'";
+                break;
+            default:
+                initialLetterConvert = "";
+        }
+        return basicSelect + initialLetterConvert;
+    }
 }
+
+
 
 
 
--- src/main/java/VerbServlet.java
+++ src/main/java/VerbServlet.java
@@ -1,17 +1,14 @@
-import java.io.*;
-import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Array;
 import java.sql.SQLException;
 import java.util.List;
+
 //import com.google.gson.Gson;
 
 @WebServlet(name = "VerbServlet", urlPatterns = "/verbs")
@@ -23,6 +20,10 @@ public class VerbServlet extends HttpServlet {
         response.setContentType("application/json");
         String verbID = request.getParameter("id");
         System.out.println(verbID);
+// Set up HTML: One div 3 columns wide containing the complete list of verbs,
+// (each verb gets a span, each span is separated with a <br/>
+// then one div 6 columns wide to display information on the verb selected.
+// The macronToCircumflex function converts macrons (which don't print) to circumflex accents.
         try {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
@@ -32,37 +33,52 @@ public class VerbServlet extends HttpServlet {
                     "    <meta charset=\"UTF-8\"><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css\"> <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\"></script></head><body>");
             out.println("<div id=\"verb-container\" class=\"container-flex\">");
             out.println("<div id=\"verb-row\" class=\"row\">");
-            out.println("<div id=\"verb-list\" class=\"col-3\">");
+            out.println("<div id=\"verb-list\" class=\"col-3\"><p>");
+// Loop through and display all the first and second principal parts of each verb;
+// each span gets a data-id attribute from the ID of its verb.
+// Then close out the verb-list div.
             for (Verb v : verbs) {
-                out.println("<p data-id=" + v.getId() + "><a href=\"/verbs?id=" + v.getId() + "\">" + macronToCircumflex(v.getFirstPart()) + ", " + macronToCircumflex(v.getSecondPart()) + "</a></p>");
+                out.println("<span data-id=" + v.getId() + "><a href=\"/verbs?id=" + v.getId() + "\">" + macToCirc(v.getFirstPart()) + ", " + macToCirc(v.getSecondPart()) + "</a></span><br/>");
             }
             out.println("</div>");
+// When user clicks a verb, it runs the findOne method to select a single verb
+// corresponding to the ID of the verb selected.
+// Various criteria determine whether the verb has third and fourth principal parts
+// or is deponent and what fields are relevant to display.
             if (verbID != null) {
                 int verbIDAsInteger = Integer.parseInt(verbID);
                 Verb thisVerb = dao.findOne(verbIDAsInteger);
                 out.println("<div id=\"verb-detail\" class=\"col-6\">");
-                out.print("<p style=\"text-size: large\">" + thisVerb.getFirstPart() + ", " + thisVerb.getSecondPart() + ", ");
+                out.print("<p style=\"text-size: large\">" + macToCirc(thisVerb.getFirstPart()) + ", " + macToCirc(thisVerb.getSecondPart()) + ", ");
+// Display third principal part if the verb has one; otherwise ---.
                 if (thisVerb.getThirdPart() == null) {
                     out.print ("---");
                 } else {
-                    out.print(thisVerb.getThirdPart());
+                    out.print(macToCirc(thisVerb.getThirdPart()));
                 }
-                if (thisVerb.getDeponency() != "Deponent") {
+// Display fourth principal part if the verb has one; otherwise ---.
+// Deponent and semi-deponent verbs don't have the fourth part.
+                if (thisVerb.getDeponency() == null) {
                     if (thisVerb.getSupine() == null) {
                         out.print (", ---");
                     } else {
-                        out.print(", " + thisVerb.getSupine());
+                        out.print(", " + macToCirc(thisVerb.getSupine()));
                     }
                 }
                 out.print ("</p>");
                 out.println();
-                out.print("Conjugation " + thisVerb.getConjugation());
+// Below the principal parts, display the verb's conjugation, deponency if any,
+// English meaning(s), and any notes about it.
+                out.println("<p>Conjugation " + thisVerb.getConjugation() + "</p>");
                 if (thisVerb.getDeponency() != null) {
                     out.print(thisVerb.getDeponency());
                 }
                 out.println();
-                out.println(thisVerb.getEnglishMeanings());
-                out.println(thisVerb.getNotes());
+                out.println("<p>" + thisVerb.getEnglishMeanings() + "</p>");
+                if (thisVerb.getNotes() != null) {
+                    out.println("<p>" + thisVerb.getNotes() + "</p>");
+                }
+// Close out the verb-detail div, the body, and the html.
                 out.println("</div></body></html>");
             }
 
@@ -71,7 +87,7 @@ public class VerbServlet extends HttpServlet {
         }
     }
 
-    private String macronToCircumflex (String stringIn) {
+    private String macToCirc(String stringIn) {
         String stringOut = stringIn.replace("ā", "â");
         stringOut = stringOut.replace("ē", "ê");
         stringOut = stringOut.replace("ī", "î");
@@ -79,4 +95,5 @@ public class VerbServlet extends HttpServlet {
         stringOut = stringOut.replace("ū", "û");
         return stringOut;
     }
+
 }
